6e3e7e9daa4d38700411a1184ef450f65d5b6264
nchlscrawley <97699039+nchlscrawley@users.noreply.github.com>
2022-02-04 16:51:51 -0800

    added files, lib

---
+++ SkillDemoTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    public void multiplication() {
+        assertEquals(25, SkillDemo.multiply(5, 4));
+    }
+}
