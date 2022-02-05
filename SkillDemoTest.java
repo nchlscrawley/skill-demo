0550c6486d11b74897cdc5b14d34735e28c44ef6
nchlscrawley <97699039+nchlscrawley@users.noreply.github.com>
2022-02-04 16:54:58 -0800

    Update SkillDemoTest.java

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -2,6 +2,7 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class SkillDemoTest {
+    @Test
     public void multiplication() {
         assertEquals(25, SkillDemo.multiply(5, 5));
     }
